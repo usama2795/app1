ec8cd332acdece9ef3b5b80219a9bbf617c062f8
usama2795 <usamashahid558@gmail.com>
2025-03-01 16:36:54 +0500

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,25 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, my name is usama and this is my app!"
+    return """
+    <html>
+        <head>
+            <style>
+                body {
+                    background-color: #282c34;
+                    color: white;
+                    font-family: Arial, sans-serif;
+                    text-align: center;
+                    padding-top: 20%;
+                }
+            </style>
+        </head>
+        <body>
+            <h1>Hello, my name is Usama and this is my app!</h1>
+        </body>
+    </html>
+    """
 
 if __name__ == '__main__':
     app.run(debug=True)
+
