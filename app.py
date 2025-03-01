2b607f3cc0f72de87e5e2af29619193e8c1107b7
usama2795 <usamashahid558@gmail.com>
2025-03-01 15:56:28 +0500

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, my name is Mubeen and this is my app!"
+    return "Hello, my name is usama and this is my app!"
 
 if __name__ == '__main__':
     app.run(debug=True)
